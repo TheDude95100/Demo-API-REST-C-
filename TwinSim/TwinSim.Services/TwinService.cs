f582d4931ff232dcbc329fb9e662726dda86d38d
TheDude95100 <hcalvet70@gmail.com>
2025-06-03 21:37:29 -0400

    Update TwinService.cs

---
+++ TwinSim/TwinSim.Services/TwinService.cs
@@ -0,0 +1,28 @@
+using TwinSim.Domain.Interfaces;
+using TwinSim.Domain.Models;
+
+namespace TwinSim.Services
+{
+    public class TwinService : ITwinService
+    {
+        private readonly Dictionary<Guid, TwinObject> _store = new();
+
+        public IEnumerable<TwinObject> GetAll() => _store.Values;
+
+        public TwinObject? GetById(Guid id) => _store.GetValueOrDefault(id);
+
+        public TwinObject Create (TwinObject obj)
+        {
+            _store[obj.Id] = obj;
+            return obj;
+        }
+
+        public bool Update(Guid id, TwinObject obj)
+        {
+            if (!_store.ContainsKey(id)) return false;
+            _store[id] = obj with { Id = id };
+            return true;
+        }
+
+        public bool Delete(Guid id)  => _store.Remove(id);
+}
