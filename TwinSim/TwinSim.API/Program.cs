ee18fed6c6c19c21eac033dbddbe32a1e46ef911
TheDude95100 <hcalvet70@gmail.com>
2025-06-03 21:55:56 -0400

    Ptite demo quand même

--- TwinSim/TwinSim.API/Program.cs
+++ TwinSim/TwinSim.API/Program.cs
@@ -10,6 +10,33 @@ builder.Services.AddSingleton<ITwinService, TwinService>();
 
 var app = builder.Build();
 
+FakeDate(app.Services.GetRequiredService<ITwinService>());
+
 app.MapControllers();
 
 app.Run();
+
+
+static void FakeDate(ITwinService service)
+{
+    service.Create(new TwinObject
+    {
+        Name = "Drone A1",
+        Position = new Position(10, 0, 5),
+        Status = TwinStatus.Active
+    });
+
+    service.Create(new TwinObject
+    {
+        Name = "Capteur B2",
+        Position = new Position(-3, 2, 1),
+        Status = TwinStatus.Inactive
+    });
+
+    service.Create(new TwinObject
+    {
+        Name = "Bras Mécanique C3",
+        Position = new Position(0, 0, 0),
+        Status = TwinStatus.Error
+    });
+}
